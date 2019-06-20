0c1bf6dd8b34910fd2201445e980ec75bd9a9df6
calindner <calindner.iv@gmail.com>
2019-06-20 10:42:25 -0700

    finished assignment

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,101 @@
-Print("test")
+import os
+import csv
+
+date, revenue = ([] for i in range(2))
+
+# input and output files
+input_file = "budget-data.csv"
+output_file = "budget_data_1_summary.txt"
+
+# input and output paths
+csv_input_path = os.path.join(input_file)
+txt_output_path = os.path.join(output_file)
+
+
+with open(csv_input_path, mode='r', newline='') as budget_data:
+    reader = csv.reader(budget_data, delimiter=',')
+
+    next(reader)
+
+    row_num = 0
+    for row in reader:
+        date.append(row[0])
+        revenue.append(row[1])
+        row_num += 1
+
+
+
+
+# *-----------------*
+# |  Summary Table  |
+# *-----------------*
+
+# print summary header
+print("\nFinancial Analysis", "\n" + "-" * 50)
+
+# sum of months
+print("Total Months:", row_num)
+
+
+# sum of revenue
+revenue_sum = 0
+for i in revenue:
+    revenue_sum += int(i)
+
+print("Total Revenue: $" + str(revenue_sum))
+
+
+# average revenue change
+total_revenue_change = 0
+for h in range(row_num):
+    total_revenue_change += int(revenue[h]) - int(revenue[h - 1])
+
+# the first_pass variable is created to remove the first iteration revenue change
+# which, takes the first list element and subtracts it by the last list element.
+first_pass = (int(revenue[0]) - int(revenue[-1]))
+total_revenue_change_adj = total_revenue_change - first_pass
+
+avg_revenue_change = (total_revenue_change_adj + int(revenue[0])) / row_num
+print("Average Revenue Change: $" + str(round(avg_revenue_change)))
+
+
+# greatest increase in revenue
+high_revenue = 0
+for j in range(len(revenue)):
+    if int(revenue[j]) - int(revenue[j - 1]) > high_revenue:
+        high_revenue = int(revenue[j]) - int(revenue[j - 1])
+        high_month = date[j]
+
+print("Greatest Increase in Revenue:", high_month, "($" + str(high_revenue) + ")")
+
+
+# greatest decrease in revenue
+low_revenue = 0
+for k in range(len(revenue)):
+    if int(revenue[k]) - int(revenue[k - 1]) < low_revenue:
+        low_revenue = int(revenue[k]) - int(revenue[k - 1])
+        low_month = date[k]
+
+print("Greatest Decrease in Revenue:", low_month, "($" + str(low_revenue) + ")")
+
+
+# white space after table
+print("\n\n")
+
+
+# *-------------------*
+# |  Output TXT File  |
+# *-------------------*
+
+with open(txt_output_path, mode='w', newline='') as summary_txt:
+    writer = csv.writer(summary_txt)
+
+    writer.writerows([
+        ["Financial Analysis for: " + input_file],
+        ["-" * 50],
+        ["Total Months: " + str(row_num)],
+        ["Total Revenue: " + "${:,.0f}".format(revenue_sum)],
+        ["Average Revenue Change: " + "${:,.0f}".format(round(avg_revenue_change))],
+        ["Greatest Increase in Revenue: " + str(high_month) + " ($" + str(high_revenue) + ")"],
+        ["Greatest Decrease in Revenue: " + str(low_month) + " ($" + str(low_revenue) + ")"]
+    ])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,64 @@
-Print("test")
+
+
+import os
+import csv
+
+output_file = "Election.txt"
+txt_output_path = os.path.join(output_file)
+
+
+total_votes = 0
+candidate = ""
+candidate_votes = {}
+candidate_percentages ={}
+winner_votes = 0
+winner = ""
+
+# open csv file
+filepath = os.path.join("election_data.csv")
+with open(filepath,'r', newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    next(csvreader)
+
+    # tally votes
+    for row in csvreader:
+        total_votes = total_votes + 1
+        candidate = row[2]
+        if candidate in candidate_votes:
+            candidate_votes[candidate] = candidate_votes[candidate] + 1
+        else:
+            candidate_votes[candidate] = 1
+
+# calculate vote percentage and identify winner
+for person, vote_count in candidate_votes.items():
+    candidate_percentages[person] = '{0:.0%}'.format(vote_count / total_votes)
+    if vote_count > winner_votes:
+        winner_votes = vote_count
+        winner = person
+
+dashbreak = "-------------------------"
+
+# print out results
+print("Election Results")
+print(dashbreak)
+print(f"Total Votes: {total_votes}")
+print(dashbreak)
+for person, vote_count in candidate_votes.items():
+    print(f"{person}: {candidate_percentages[person]} ({vote_count})")
+print(dashbreak)
+print(f"Winner: {winner}")
+print(dashbreak)
+
+# save summary to txt
+
+with open(txt_output_path, mode='w', newline='') as POLsummary_txt:
+    writer = csv.writer(POLsummary_txt)
+    POLsummary_txt.write(dashbreak + "\n")
+    POLsummary_txt.write(f"Total Votes: {total_votes}" + "\n")
+    POLsummary_txt.write(dashbreak + "\n")
+    for person, vote_count in candidate_votes.items():
+        POLsummary_txt.write(f"{person}: {candidate_percentages[person]} ({vote_count})" + "\n")
+    POLsummary_txt.write(dashbreak + "\n")
+    POLsummary_txt.write(f"Winner: {winner}" + "\n")
+    POLsummary_txt.write(dashbreak + "\n")
